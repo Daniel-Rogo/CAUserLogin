7c8609c43f109a1496daa7bda75bf138d030f4a8
Daniel-Rogo <daniel.r2017@icloud.com>
2024-10-22 17:21:31 -0400

    Task 2

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,4 +27,9 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * No clue what it is supposed to return.
+     * @param name the username to look up
+     */
+    void setCurrentUser(String name);
 }
